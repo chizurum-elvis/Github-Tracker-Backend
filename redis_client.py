ce8f95604de6ef3454dffd321b8083dae9a08a7a
Chizurum <okereolujiechizurum@gmail.com>
2025-06-30 11:03:50 +0100

    Initial commit

--- redis_client.py
+++ redis_client.py
@@ -1,7 +1,14 @@
 import redis
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
 REDIS_URL = os.getenv("REDIS_URL")
+
+if not REDIS_URL:
+    raise ValueError("Missing REDIS_URL environment variable")
+
 redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
