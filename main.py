bce7d40bf626cf263a343df57b602c7c8673e808
Chizurum <okereolujiechizurum@gmail.com>
2025-07-02 10:00:03 +0100

    Initial commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[frontend_url],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT"],
     allow_headers=["*"],
 )
 
